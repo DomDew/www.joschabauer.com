4578f3dc3a599887313e8a49a863e418661dadba
Dominik Dewitz <dewitzdominik@kabelmail.de>
2021-08-05 12:02:30 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Joscha Bauer Portfolio",
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-gatsby-cloud"],
+};
