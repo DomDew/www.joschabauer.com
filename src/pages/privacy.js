5da16aa4518ca9addc16a8b866b5b72c9289db6c
Dominik Dewitz <dewitzdominik@kabelmail.de>
2021-08-06 15:18:12 +0200

    Change styling of hamburger before

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -1,7 +1,6 @@
 import { AnimatePresence } from 'framer-motion'
 import React from 'react'
 import PrivacyCard from '../components/PrivacyCard'
-import Layout from '../components/Layout'
 
 export default function Privacy() {
   return (
